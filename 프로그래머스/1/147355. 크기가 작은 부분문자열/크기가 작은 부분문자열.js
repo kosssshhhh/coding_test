3bc8b1fef624242a36213beb72e31bee8e318a52
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-02-01 16:46:41 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,3 +1,4 @@
+// 내 풀이
 function solution(t, p) {
   let res = 0;
 
@@ -7,3 +8,14 @@ function solution(t, p) {
 
   return res;
 }
+
+// 참고 풀이
+// 문자열 앞에 + 를 붙이면 숫자로 변환됨
+// function solution(t, p) {
+//   let count = 0;
+//   for (let i = 0; i <= t.length - p.length; i++) {
+//     let value = t.slice(i, i + p.length);
+//     if (+p >= +value) count++;
+//   }
+//   return count;
+// }
