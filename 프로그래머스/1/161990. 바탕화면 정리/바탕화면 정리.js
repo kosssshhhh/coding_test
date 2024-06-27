f6eaeee88ca676646dcb7213fbc0b9b4ac96f856
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-06-27 21:01:45 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,22 @@
+function solution(wallpaper) {
+  var answer = [];
+  var [lux, luy] = [wallpaper.length, wallpaper[0].length];
+  var [rdx, rdy] = [1, 1];
+
+
+  wallpaper.forEach((a, i) => {
+    a.split('').forEach((b, j)=>{
+        if(b === '#'){
+            lux = Math.min(lux, i)
+            luy = Math.min(luy, j)
+            rdx = Math.max(rdx, i+1)
+            rdy = Math.max(rdy, j+1)
+        }
+
+    })
+  });
+    
+    answer = [lux, luy, rdx, rdy]
+
+  return answer;
+}
