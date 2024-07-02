d8471eb201263dab309325922ac1cea2fa11bf69
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-07-02 13:18:28 +0900

    [level 1] Title: 둘만의 암호, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,27 +1,24 @@
 function solution(s, skip, index) {
     let answer = '';
 
-    for (let i = 0; i < s.length; i++) {
-        let currentChar = s[i];
-        let shifts = 0;
-
-        while (shifts < index) {
-            currentChar = String.fromCharCode(currentChar.charCodeAt(0) + 1);
-
-            // 'z'를 넘어가면 'a'로 다시 시작
-            if (currentChar > 'z') {
-                currentChar = 'a';
+    for(let i=0; i<s.length; i++){
+        let currStr = s[i];
+        let shift = 0;
+        
+        while(shift < index){
+            currStr = String.fromCharCode(currStr.charCodeAt(0) + 1);
+            
+            if(currStr > 'z'){
+                currStr = 'a'
             }
-
-            // skip에 포함되지 않는 문자만 유효한 이동으로 간주
-            if (!skip.includes(currentChar)) {
-                shifts++;
+            
+            if(!skip.includes(currStr)){
+                shift++
             }
+            
         }
-
-        answer += currentChar;
+        answer += currStr
     }
-
     return answer;
 }
 
