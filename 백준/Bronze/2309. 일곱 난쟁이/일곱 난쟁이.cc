e16cb197eac7195bbe5c39d563c8d53498144da6
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-12 19:28:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,33 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n = 9, k = 2;
+int a[9];
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
-  vector<int> v(9);
-  int sum = 0;
-
-  for (int i = 0; i < n; i++) {
-    cin >> v[i];
-    sum += v[i];
+  for (int i = 0; i < 9; i++) {
+    cin >> a[i];
   }
+  sort(a, a + 9);
 
-  sort(v.begin(), v.end());
+  do {
+    int sum = 0;
+    for (int i = 0; i < 7; i++) sum += a[i];
+    if (sum == 100) break;
+  } while (next_permutation(a, a + 9));
 
-  for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (sum - v[i] - v[j] == 100) {
-        for (int k = 0; k < n; k++) {
-          if (k != i && k != j) cout << v[k] << '\n';
-        }
-        return 0; // 원하는 결과를 찾으면 프로그램 종료
-      }
-    }
-  }
-
+  for (int i = 0; i < 7; i++) cout << a[i] << '\n';
+  
   return 0;
 }
