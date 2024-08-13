736bd76a086fe22c13ad96cefc04a5b0238d7a6c
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-13 13:45:43 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,46 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int A, B, C;
-int first, last;
-int min_t = 101, max_t = 0;
-int sum = 0;
-vector<pair<int, int>> v;
+int A, B, C, a, b, cnt[104], ret;
 
 int main() {
   cin >> A >> B >> C;
   for (int i = 0; i < 3; i++) {
-    cin >> first >> last;
-
-    v.push_back({first, last});
-    if (min_t > first) min_t = first;
-    if (max_t < last) max_t = last;
+    cin >> a >> b;
+    for (int i = a; i < b; i++) cnt[i]++;
   }
 
-  int cnt[max_t - min_t];  // cnt 배열 생성
-  memset(cnt, 0, sizeof(cnt));
-
-  for (pair<int, int> a : v) {
-    for (int i = a.first - min_t; i < a.second - min_t; i++) {
-      cnt[i]++;
+  for (int j = 1; j < 100; j++) {
+    if (cnt[j]) {
+      if (cnt[j] == 1)
+        ret += A;
+      else if (cnt[j] == 2)
+        ret += B * 2;
+      else if (cnt[j] == 3)
+        ret += C * 3;
     }
   }
 
-  for (int i : cnt) {
-    if (i == 3)
-      sum += C * i;
-    else if (i == 2)
-      sum += B * i;
-    else if (i == 1)
-      sum += A * i;
-  }
-
-  cout << sum << '\n';
-
-  // 1. min max 구하기
-  // 2. 시간 배열 생성 (크기 = max - min)
-  // 3. v 돌면서 0~max-min 까지 값 채우기
-  // 4. sum 하는 로직
-
+  cout << ret << '\n';
   return 0;
 }
