82bc79d537ca322e26bf2d98099485309e51b494
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-14 16:24:12 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -2,53 +2,28 @@
 using namespace std;
 
 int N;
-string pattern, temp, str1, str2;
-vector<string> v, s;
-
-vector<string> split(string input, string delimeter) {
-  long long pos = 0;
-  string token = "";
-  vector<string> ret;
-
-  while ((pos = input.find(delimeter)) != string::npos) {
-    token = input.substr(0, pos);
-    ret.push_back(token);
-    input.erase(0, pos + delimeter.length());
-  }
-  ret.push_back(input);
-
-  return ret;
-}
+string pat, s;
 
 int main() {
-  cin >> N >> pattern;
+  cin >> N >> pat;
+
+  int pos = pat.find('*');
+  string pre = pat.substr(0, pos);
+  string suf = pat.substr(pos + 1);
 
   for (int i = 0; i < N; i++) {
-    cin >> temp;
-    v.push_back(temp);
-  }
+    cin >> s;
 
-  s = split(pattern, "*");
-
-  for (string i : v) {
-    str1 = "";
-    str2 = "";
-    if (s[0].length() > i.length() || s[1].length() > i.length()) {
-      cout << "NE" << '\n';
-      continue;
+    if (s.size() < pre.size() + suf.size()) {
+      cout << "NE\n";
+    } else {
+      if (s.substr(0, pre.size()) == pre &&
+          s.substr(s.size() - suf.size()) == suf) {
+        cout << "DA\n";
+      } else {
+        cout << "NE\n";
+      }
     }
-    if (i.length() < s[0].length() + s[1].length()) {
-      cout << "NE" << '\n';
-      continue;
-    }
-
-    str1 = i.substr(0, s[0].length());
-    str2 = i.substr(i.length() - s[1].length(), s[1].length());
-
-    if (str1 == s[0] && str2 == s[1])
-      cout << "DA" << '\n';
-    else
-      cout << "NE" << '\n';
   }
 
   return 0;
