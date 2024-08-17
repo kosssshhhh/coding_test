f735b28109c2102593d61274b74317408c9b499d
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-17 13:00:45 +0900

    [Silver IV] Title: 주몽, Time: 120 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -8,15 +8,23 @@ int arr[15004];
 int main() {
   cin >> n >> m;
 
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
+  for (int i = 0; i < n; i++) cin >> arr[i];
+
+  // 문제 조건에 따라 불가능한 조건
+  if (m > 200000)
+    cout << 0 << "\n";
+
+  else {
+    for (int i = 0; i < n; i++)
+      for (int j = i + 1; j < n; j++) {
+        if (m == arr[i] + arr[j]) ret++;
+      }
+    cout << ret << '\n';
   }
 
-  for (int i = 0; i < n; i++)
-    for (int j = i + 1; j < n; j++) {
-      if (m == arr[i] + arr[j]) ret++;
-    }
-
-  cout << ret << '\n';
   return 0;
 }
+
+// 두 개를 선택하여 ~ -> 조합
+// 문제의 조건에서 일어날 수 없는 경우를 예외처리해준다.
+// 시간초과가 빡빡한 문제의 경우 이러한 조건을 생각해주어야할 필요도 있음.
