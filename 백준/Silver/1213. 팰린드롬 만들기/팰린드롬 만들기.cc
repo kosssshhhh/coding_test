939d2ce63d65858b9a8d7abac72913869fa2dcbe
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-17 12:18:46 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -9,9 +9,6 @@ vector<char> ret;
 int main() {
   cin >> input;
 
-  // for (int i = 0; i < input.length(); i++) {
-  //   mp[input[i]]++;
-  // }
   for (char c : input) {
     mp[c]++;
   }
@@ -31,20 +28,7 @@ int main() {
     }
   }
 
-  // for (auto i : mp) cout << i.first << " : " << i.second << '\n';
-
   // Map 거꾸로 순회하여 오름차순, ret 배열의 앞 뒤로 삽입.
-  // for (auto it = mp.rbegin(); it != mp.rend(); it++) {
-  //   for (int i = 0; i < (*it).second; i++) {
-  //     if ((*it).second == 0) break;
-
-  //     // cout << (*it).first << " : " << (*it).second << '\n';
-  //     ret.push_back((*it).first);
-  //     ret.insert(ret.begin(), (*it).first);
-  //     (*it).second -= 2;
-  //   }
-  // }
-
   for (auto it = mp.rbegin(); it != mp.rend(); it++) {
     while ((*it).second > 1) {
       ret.push_back((*it).first);
@@ -60,3 +44,6 @@ int main() {
 
   return 0;
 }
+
+// Map을 활용하여 풀이
+// cnt 배열을 활용해서 풀 수도 있음
