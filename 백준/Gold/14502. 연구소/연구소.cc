805acfcdd9d080779ce63c151ecf4559363ae525
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-08-28 23:16:09 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int m, n, cnt, ret = 0, wall_cnt; // ret 초기값을 0으로 수정
+int m, n, cnt, ret = 100, wall_cnt;
 int a[10][10], visited[10][10];
 vector<pair<int, int>> v;
 
@@ -15,8 +15,7 @@ void dfs(int y, int x) {
     int ny = y + dy[i];
     int nx = x + dx[i];
 
-    // 배열 범위 체크 조건 수정: '> m' -> '>= m' 그리고 '> n' -> '>= n'
-    if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue; // 수정된 부분
+    if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
     if (a[ny][nx] == 0 && visited[ny][nx] == 0) {
       dfs(ny, nx);
     }
@@ -42,7 +41,6 @@ int main() {
         a[v[j].first][v[j].second] = 1;
         a[v[k].first][v[k].second] = 1;
 
-        cnt = 0; // cnt 초기화 위치 변경 (여기서 초기화해야 정확한 카운트가 됨)
         for (int l = 0; l < m; l++) {
           for (int o = 0; o < n; o++) {
             if (a[l][o] == 2 && visited[l][o] == 0) {
@@ -50,19 +48,16 @@ int main() {
             }
           }
         }
-
-        // ret 값을 업데이트하는 로직 수정 (최대 안전 영역을 계산)
-        ret = max(m * n - cnt - wall_cnt - 3, ret); // 수정된 부분
-
-        memset(visited, 0, sizeof(visited)); // visited 배열 초기화
+        ret = min(cnt, ret);
+        cnt = 0;
+        memset(visited, 0, sizeof(visited));
         a[v[i].first][v[i].second] = 0;
         a[v[j].first][v[j].second] = 0;
         a[v[k].first][v[k].second] = 0;
       }
     }
   }
-  
-  cout << ret << '\n'; // 최대 안전 영역 출력
+  cout << m * n - ret - wall_cnt - 3 << '\n';
 
   return 0;
 }
