566fad64d67066dc1fb214dc379e37177706c71d
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-09-06 21:49:38 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -3,28 +3,22 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
+int n, cnt, mx, a[104][104], visited[104][104];
 
-int n, cnt, ret = 1, temp, h;
-int a[104][104], visited[104][104];
-
-void dfs(int y, int x) {
+void dfs(int y, int x, int h) {
   visited[y][x] = 1;
 
   for (int i = 0; i < 4; i++) {
     int ny = y + dy[i];
     int nx = x + dx[i];
-    if (ny < 0 || ny > n || nx < 0 || nx > n || a[ny][nx] <= h) continue;
-    if (visited[ny][nx]) continue;
 
-    if (a[ny][nx] > h && !visited[ny][nx]) dfs(ny, nx);
+    if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]) continue;
+    if (a[ny][nx] <= h) continue;
+    dfs(ny, nx, h);
   }
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
   cin >> n;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
@@ -32,31 +26,23 @@ int main() {
     }
   }
 
-  for (int k = 0; k <= 100; k++) {
+  for (int h = 1; h <= 100; h++) {
     cnt = 0;
     memset(visited, 0, sizeof(visited));
-    h = k;
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-        if (visited[i][j] || a[i][j] <= h) continue;
+        if (a[i][j] <= h || visited[i][j]) continue;
+        dfs(i, j, h);
         cnt++;
-        dfs(i, j);
       }
     }
-
-    // for (int i = 0; i < n; i++) {
-    //   for (int j = 0; j < n; j++) {
-    //     cout << visited[i][j] << ' ';
-    //   }
-    //   cout << '\n';
-    // }
-
-    // cout << '\n';
-    ret = max(ret, cnt);
+    mx = max(cnt, mx);
   }
-
-  cout << ret << '\n';
-
+  if (mx == 0) {
+    cout << 1;
+    return 0;
+  }
+  cout << mx;
   return 0;
 }
