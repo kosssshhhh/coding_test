d2e4cfa6d2905384e4b114a7955658b5b3646435
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-09-13 15:10:41 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x, ret;
+
+int main() {
+  cin >> x;
+
+  for (int i = 0; i < 7; i++) {
+    if (x & (1 << i)) {
+      ret++;
+    }
+  }
+
+  cout << ret;
+
+  return 0;
+}
