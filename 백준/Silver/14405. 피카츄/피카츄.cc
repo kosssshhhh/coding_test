2b0e4b12f457665624b1c7286cf4a6f108493da8
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-09-20 22:18:08 +0900

    [Silver V] Title: 피카츄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.cc
+++ 백준/Silver/14405. 피카츄/피카츄.cc
@@ -6,34 +6,19 @@ string s;
 int main() {
   cin >> s;
 
-  for (int i = 0; i < s.length(); i++) {
-    if (s[i] == 'p') {
-      if (s[i + 1] == 'i') {
-        i++;
-      } else {
-        cout << "NO";
-        return 0;
-      }
-    } else if (s[i] == 'k') {
-      if (s[i + 1] == 'a') {
-        i++;
-      } else {
-        cout << "NO";
-        return 0;
-      }
-    } else if (s[i] == 'c') {
-      if (s[i + 1] == 'h' && s[i + 2] == 'u') {
-        i += 2;
-      } else {
-        cout << "NO";
-        return 0;
-      }
+  for (int i = 0; i < s.size(); i++) {
+    if (i < s.size() - 1 &&
+        (s.substr(i, 2) == "pi" || s.substr(i, 2) == "ka")) {
+      i += 1;
+    } else if (i < s.size() - 2 && s.substr(i, 3) == "chu") {
+      i += 2;
     } else {
       cout << "NO";
       return 0;
     }
   }
   cout << "YES";
-
   return 0;
 }
+
+// s.substr(0, 2) - 문자열 s의 0번째 인덱스부터 2개 뽑기
