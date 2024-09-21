b3836e0b59069e9cbea9325fc778dd335c56cbcc
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-09-21 16:23:00 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 8 ms, Memory: 3844 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -9,50 +9,31 @@ int main() {
   cin >> n >> s;
 
   for (int i = 0; i < n; i++) {
-    if (s[i] == '(') {
+    if (stk.empty()) {
       stk.push(i);
-    } else {
-      if (stk.empty()) {
-        stk.push(i);
-      } else {
-        if (s[stk.top()] == '(') {
-          arr[stk.top()] = 1;
-          arr[i] = 1;
-          stk.pop();
-        } else {
-          stk.push(i);
-        }
-      }
+      continue;
     }
-    mx = max(mx, cnt);
+    if (s[stk.top()] == '(' && s[i] == ')') {
+      arr[stk.top()] = 1;
+      arr[i] = 1;
+      stk.pop();
+    } else {
+      stk.push(i);
+    }
   }
-
-  // for (int i = 0; i < n; i++) {
-  //   if (stk.empty()) {
-  //     stk.push(i);
-  //     continue;
-  //   }
-  //   if (s[stk.top()] == '(' && s[i] == ')') {
-  //     arr[stk.top()] = 1;
-  //     arr[i] = 1;
-  //     stk.pop();
-  //   } else {
-  //     stk.push(i);
-  //   }
-  // }
   for (int i = 0; i < n; i++) {
     if (arr[i] == 1) {
       cnt++;
       mx = max(cnt, mx);
-    } else {
-      mx = max(cnt, mx);
+    } else
       cnt = 0;
-    }
   }
 
   cout << mx;
   return 0;
 }
 
-// '(' 얘는 일단 다 넣음
-// ')' 얘가 들어올때마다 매칭 되는게 있으면 cnt++ 하고 없으면 cnt 초기화
+// 1. 배열을 이용하는 방법
+// stack에 string의 인덱스를 넣어줌
+// match 되면, arr에 해당 index 1로 바꿈
+// arr의 연속된 1을 구해준다.
