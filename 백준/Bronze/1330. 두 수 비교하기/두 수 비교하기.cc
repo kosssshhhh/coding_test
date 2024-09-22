3db5b1afb21ed2b2e1bb7a578bf985b8c982ae32
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-09-22 13:59:24 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B;
+
+int main(){
+    cin >> A >> B;
+    if(A > B) cout << '>';
+    else if(A < B) cout << '<';
+    else cout << "==";
+    
+    return 0;
+}
