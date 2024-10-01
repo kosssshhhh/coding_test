a2355c0cbccedd8a217e84a32ce94fff5eefc1bf
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-01 17:27:33 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B;
+
+int main() {
+  while (1) {
+    cin >> A >> B;
+    if (A == 0 && B == 0) break;
+    cout << A + B << '\n';
+  }
+
+  return 0;
+}
