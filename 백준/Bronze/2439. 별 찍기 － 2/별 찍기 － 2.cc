9a30d47c126790e93f9db1abbe421dabfcd2167e
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-01 17:26:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    for (int j = N - i - 1; j > 0; j--) cout << ' ';
+    for (int j = 0; j < i + 1; j++) cout << '*';
+    cout << '\n';
+  }
+
+  return 0;
+}
