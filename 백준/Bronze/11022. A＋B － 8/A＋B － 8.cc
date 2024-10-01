831602757dfd7ccc907e4a42486d4a34cb5bd861
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-01 17:22:07 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B, T;
+
+int main() {
+  cin >> T;
+  for (int i = 1; i <= T; i++) {
+    cin >> A >> B;
+    cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << '\n';
+  }
+
+  return 0;
+}
