53d9c44e9b444f5309c99adb401d2b551434533a
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-01 17:08:49 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, a, b;
+
+int main() {
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    cin >> a >> b;
+
+    cout << a + b << '\n';
+  }
+
+  return 0;
+}
