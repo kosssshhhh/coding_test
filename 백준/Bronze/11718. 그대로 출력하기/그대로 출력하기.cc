283e79e41cc402d777a0e2b19c47d0942a405155
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-07 02:09:11 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int main() {
+  // string buffer_stream;
+  // getline(cin, buffer_stream);
+  while (getline(cin, s)) {
+    cout << s << '\n';
+  }
+
+  return 0;
+}
