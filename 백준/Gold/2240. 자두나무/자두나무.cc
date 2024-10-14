0413b42354fb55fee78eee984179533d4e487b9d
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-14 14:50:00 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, w, ret, b[1004], dp[1004][2][34];
+
+int go(int idx, int tree, int cnt) {
+  if (cnt < 0) return -1e9;
+  if (idx == t) return cnt == 0 ? 0 : -1e9;
+  int &ret = dp[idx][tree][cnt];
+  if (~ret) return ret;
+
+  return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) +
+               (tree == b[idx] - 1);
+}
+
+int main() {
+  memset(dp, -1, sizeof(dp));
+  cin >> t >> w;
+  for (int i = 0; i < t; i++) cin >> b[i];
+
+  cout << max(go(0, 1, w - 1), go(0, 0, w)) << '\n';
+  return 0;
+}
