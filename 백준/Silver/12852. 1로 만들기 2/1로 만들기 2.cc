178bcfda63bd4ed33178f8ef687d627e01726cfd
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-17 21:49:56 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -12,7 +12,7 @@ void go(int here) {
     go(here / 3);
   else if (here % 2 == 0 && dp[here] == dp[here / 2] + 1)
     go(here / 2);
-  else if (here - 1 >= 0 && dp[here] == dp[here - 1] + 1)
+  else if (here - 1 > 0 && dp[here] == dp[here - 1] + 1)
     go(here - 1);
 
   return;
@@ -24,7 +24,7 @@ int main() {
   fill(dp, dp + 1000004, INF);
 
   dp[1] = 0;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 2; i <= n; i++) {
     if (!(i % 3)) dp[i] = min(dp[i / 3] + 1, dp[i]);
     if (!(i % 2)) dp[i] = min(dp[i / 2] + 1, dp[i]);
     dp[i] = min(dp[i - 1] + 1, dp[i]);
@@ -32,9 +32,6 @@ int main() {
   cout << dp[n] << '\n';
 
   go(n);
+
   return 0;
 }
-
-// DP
-// 연산의 겹치는 부분이 발생하고, 부분 최적 구조를 가짐
-// trace -> 나눈 값들이 차이가 1 나는 부분을 추적함
