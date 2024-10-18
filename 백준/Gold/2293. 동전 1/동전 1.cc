43d646ee48b77c0e80d490355d7a8d9d6c245b19
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-18 22:20:40 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, temp, dp[100004];
+
+int main() {
+  cin >> n >> k;
+
+  dp[0] = 1;
+  for (int i = 1; i <= n; i++) {
+    cin >> temp;
+
+    if (temp >= 10001) continue;
+    for (int j = temp; j <= k; j++) {
+      dp[j] += dp[j - temp];
+    }
+  }
+
+  cout << dp[k];
+  return 0;
+}
