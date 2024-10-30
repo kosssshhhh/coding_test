b7c10102a7e70aa3ec51bb53d5de46b94e301f95
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-10-30 16:40:33 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 52 ms, Memory: 19996 KB -BaekjoonHub

---
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 987654321;
+int temp;
+int dp[100004][5][5];
+
+vector<int> v;
+
+int go(int here, int l, int r) {
+  if (here == v.size()) {
+    return 0;  // 종료 조건에서 INF 대신 0을 반환
+  }
+
+  int &ret = dp[here][l][r];
+  if (ret != INF) return ret;
+  ret = INF;
+
+  int l_score = 0, r_score = 0;
+
+  // 왼발 움직임에 따른 점수 계산
+  if (l == v[here]) {
+    l_score = 1;
+  } else if (l == 0) {
+    l_score = 2;
+  } else if ((abs(l - v[here]) % 2) == 1) {
+    l_score = 3;
+  } else {
+    l_score = 4;
+  }
+
+  // 오른발 움직임에 따른 점수 계산
+  if (r == v[here]) {
+    r_score = 1;
+  } else if (r == 0) {
+    r_score = 2;
+  } else if ((abs(r - v[here]) % 2) == 1) {
+    r_score = 3;
+  } else {
+    r_score = 4;
+  }
+
+  // 왼발로 갈 때
+  ret = min(ret, go(here + 1, v[here], r) + l_score);
+
+  // 오른발로 갈 때
+  ret = min(ret, go(here + 1, l, v[here]) + r_score);
+
+  return ret;
+}
+
+int main() {
+  while (1) {
+    cin >> temp;
+    if (temp == 0) break;
+    v.push_back(temp);
+  }
+
+  for (int i = 0; i < 100004; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      for (int k = 0; k < 5; ++k) {
+        dp[i][j][k] = INF;
+      }
+    }
+  }
+
+  cout << go(0, 0, 0);
+
+  return 0;
+}
