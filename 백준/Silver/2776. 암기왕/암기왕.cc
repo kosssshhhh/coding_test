d7ee4bf304a1fc6bf7ce3373443452289a736b1e
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-11-07 20:29:39 +0900

    [Silver IV] Title: 암기왕, Time: 1428 ms, Memory: 81572 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -1,38 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, n, m;
+int t, n, m, temp;
+unordered_map<int, int> mp;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
   cin >> t;
   while (t--) {
+    mp.clear();
+
     cin >> n;
-    vector<int> v1(n);
-    for (int i = 0; i < n; i++) cin >> v1[i];
+    for (int i = 0; i < n; i++) {
+      cin >> temp;
+      mp[temp] = 1;
+    }
     cin >> m;
-    vector<int> v2(m);
-    for (int i = 0; i < m; i++) cin >> v2[i];
+    for (int i = 0; i < m; i++) {
+      cin >> temp;
 
-    sort(v1.begin(), v1.end());
-
-    for (int val : v2) {
-      int ret = 0;
-      int lo = 0, hi = n;
-
-      while (lo <= hi) {
-        int mid = (lo + hi) / 2;
-        // cout << "val: " << val << '\n';
-        // cout << "mid: " << mid << '\n';
-        if (v1[mid] == val) {
-          ret = 1;
-          break;
-        } else if (v1[mid] > val) {
-          hi = mid - 1;
-        } else {
-          lo = mid + 1;
-        }
+      if (mp[temp]) {
+        cout << 1 << '\n';
+      } else {
+        cout << 0 << '\n';
       }
-      cout << ret << '\n';
     }
   }
 
