e58f678e02845bd8afdd02f02fd9d9f72350330a
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-11-12 13:30:45 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[1004], cnt[1004], ret;
+int n, num, lis[1004], len;
 
 int main() {
   cin >> n;
-  for (int i = 0; i < n; i++) cin >> a[i];
-
   for (int i = 0; i < n; i++) {
-    int maxValue = 0;
-    for (int j = 0; j < i; j++) {
-      if (a[j] < a[i] && maxValue < cnt[j]) maxValue = cnt[j];
-    }
-    cnt[i] = maxValue + 1;
-    ret = max(ret, cnt[i]);
+    cin >> num;
+    auto lowerPos = lower_bound(lis, lis + len, num);
+
+    if (*lowerPos == 0) len++;
+
+    *lowerPos = num;
   }
-  cout << ret;
+
+  cout << len;
+
   return 0;
 }
 
-// 최대 증가 수열, N^2 풀이
+// 최대 증가 수열, NlogN 시간복잡도 풀이
