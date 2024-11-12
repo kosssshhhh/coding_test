bd1d4c4f3bdb8bab28ac40b3ee157914b65c603a
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-11-12 13:10:44 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -2,27 +2,22 @@
 using namespace std;
 
 int n;
-double a[10004];
-
-double go() {
-  double mx = -1e9;
-  for (int start = 0; start < n; start++) {
-    double curr = 1.0;
-    for (int end = start; end < n; end++) {
-      curr *= a[end];
-      mx = max(mx, curr);
-    }
-  }
-  return mx;
-}
+double a, sum = 1.0, ret = -1;
 
 int main() {
   cin >> n;
-  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+    sum *= a;
 
-  double ret = go();
+    ret = max(sum, ret);
+    if (sum < a) {
+      sum = a;
+      ret = max(sum, ret);
+    }
+  }
 
-  cout << fixed << setprecision(3) << ret << '\n';
+  cout << fixed << setprecision(3) << ret;
 
   return 0;
 }
