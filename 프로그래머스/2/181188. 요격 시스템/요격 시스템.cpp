d5585b081dd1d945914af96c65674f0f669586b3
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-11-20 16:27:11 +0900

    [level 2] Title: 요격 시스템, Time: 213.70 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -2,35 +2,30 @@
 
 using namespace std;
 
-vector<pair<int, int>> v;
-
-bool cmp(pair<int, int> a, pair<int, int> b){
-    return a.second < b.second;
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1];
 }
+
 int solution(vector<vector<int>> targets) {
     int answer = 0;
-    int n = targets.size();
     
-    for (vector<int> a : targets){
-        v.push_back({a[0], a[1]});
-    }
+    sort(targets.begin(), targets.end(), cmp);
     
-    sort(v.begin(), v.end(), cmp);
+    int left = 0, right = -1;
     
-    int left = v[0].first, right = v[0].second;
-    int ret = 1;
-    
-    for (int i=1; i<n; i++){
-        if(right <= v[i].first){
-            left = v[i].first;
-            right = v[i].second;
-            ret++;
+    for (auto a : targets){
+        if(right <= a[0]){
+            answer++;
+            right = a[1];
         }
     }
-    
-    return ret;
+    return answer;
 }
 
 // 1. 시작 지점 기반으로 정렬 오름차순 정렬
 // 2. 반복문 돌면서
 // 2-1. 이전 끝나는 시간보다 다음 시작하는 시간이 더 크다면 left rigth 갱신, ret++
+
+// 그리디, 정렬
+// 끝나는 지점 기반으로 오름차순 정렬
+// right 값과 a[0]값을 비교하여 업데이트
