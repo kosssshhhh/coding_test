9b48e52d8ee69e3b787078dc7e3582d3e5b3a6d5
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2024-12-23 17:14:46 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, psum[100004], temp, ret = -10000000;
+int n, k, ret = -1e9, psum[100004];
 
 int main() {
-  cin >> N >> K;
+  cin >> n >> k;
 
-  for (int i = 1; i <= N; i++) {
+  for (int i = 1; i <= n; i++) {
+    int temp;
     cin >> temp;
     psum[i] = psum[i - 1] + temp;
   }
 
-  for (int i = K; i <= N; i++) {
-    ret = max(ret, psum[i] - psum[i - K]);
+  for (int i = k; i <= n; i++) {
+    ret = max(ret, psum[i] - psum[i - k]);
   }
 
   cout << ret;
 
   return 0;
 }
-
-// 누적합(psum)을 이용한 풀이
-// 구간합 같은 경우에는 누적합 배열을 사용하여 푸는 것이 좋다!
