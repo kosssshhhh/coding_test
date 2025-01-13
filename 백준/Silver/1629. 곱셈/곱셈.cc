42d500b2e30555e3b63ac2ad68a76aa30e9f54fe
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-01-13 17:37:12 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
+
 ll a, b, c;
 
-long long func(ll a, ll b) {
-  if (b == 0) return 1;
+ll go(ll a, ll b) {
   if (b == 1) return a % c;
 
-  ll ret = func(a, b / 2);
+  ll ret = go(a, b / 2);
+  ret = ret * ret % c;
 
-  ret = (ret * ret) % c;
-
-  if (b % 2) ret = (ret * a) % c;
+  if (b % 2) ret = ret * a % c;
 
   return ret;
 }
@@ -20,7 +18,7 @@ long long func(ll a, ll b) {
 int main() {
   cin >> a >> b >> c;
 
-  cout << func(a, b) << '\n';
+  cout << go(a, b);
 
   return 0;
 }
