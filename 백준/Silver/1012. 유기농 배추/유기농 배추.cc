8b7120a0efdbae7c8e4e0e2b022c2fbf4909e0fb
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-01-15 19:31:10 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -3,21 +3,18 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
+int t, m, n, k, y, x, ret, a[54][54], visited[54][54];
 
-int t, m, n, k, x, y, ret;
-int a[54][54];
-int visited[54][54];
-
-void DFS(int y, int x) {
+void dfs(int y, int x) {
   visited[y][x] = 1;
 
   for (int i = 0; i < 4; i++) {
     int ny = y + dy[i];
     int nx = x + dx[i];
-    if (ny < 0 || ny > m || nx < 0 || nx > n || a[ny][nx] == 0) continue;
-    if (visited[ny][nx]) continue;
 
-    DFS(ny, nx);
+    if (ny < 0 || nx < 0 || ny > n || nx > m) continue;
+    if (a[ny][nx] == 0 || visited[ny][nx]) continue;
+    dfs(ny, nx);
   }
 }
 
@@ -25,24 +22,24 @@ int main() {
   cin >> t;
 
   for (int i = 0; i < t; i++) {
+    memset(visited, 0, sizeof(visited));
+    memset(a, 0, sizeof(a));
+    ret = 0;
+
     cin >> m >> n >> k;
     for (int j = 0; j < k; j++) {
-      cin >> y >> x;
+      cin >> x >> y;
       a[y][x] = 1;
     }
-
-    for (int j = 0; j < m; j++) {
-      for (int q = 0; q < n; q++) {
-        if (a[j][q] == 0 || visited[j][q]) continue;
-        ret++;
-        DFS(j, q);
+    for (int j = 0; j < n; j++) {
+      for (int l = 0; l < m; l++) {
+        if (a[j][l] && visited[j][l] == 0) {
+          dfs(j, l);
+          ret++;
+        }
       }
     }
-
     cout << ret << '\n';
-    memset(a, 0, sizeof(a));
-    memset(visited, 0, sizeof(visited));
-    ret = 0;
   }
 
   return 0;
