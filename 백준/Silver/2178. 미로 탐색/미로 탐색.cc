75e881718da486bedfa6039723dc183f8bd5f45c
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-01-15 18:51:33 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,26 +3,26 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, x, y;
-int a[104][104];
-int visited[104][104];
+int n, m, visited[104][104];
+char a[104][104];
 string temp;
 
 int main() {
   cin >> n >> m;
-
   for (int i = 0; i < n; i++) {
     cin >> temp;
     for (int j = 0; j < m; j++) {
-      a[i][j] = temp[j] - '0';
+      a[i][j] = temp[j];
     }
   }
 
   queue<pair<int, int>> q;
-  q.push({0, 0});
   visited[0][0] = 1;
+  q.push({0, 0});
 
   while (q.size()) {
+    int y, x;
+
     tie(y, x) = q.front();
     q.pop();
 
@@ -30,14 +30,19 @@ int main() {
       int ny = y + dy[i];
       int nx = x + dx[i];
 
-      if (ny < 0 || ny > n || nx < 0 || nx > m || a[ny][nx] == 0) continue;
-      if (visited[ny][nx]) continue;
-      q.push({ny, nx});
+      if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] != 0 ||
+          a[ny][nx] == '0')
+        continue;
+
       visited[ny][nx] = visited[y][x] + 1;
+      q.push({ny, nx});
     }
   }
 
-  cout << visited[n - 1][m - 1] << '\n';
+  cout << visited[n - 1][m - 1];
 
   return 0;
 }
+
+// 가중치가 같은 최단거리 
+// 기본 BFS로 풀이
