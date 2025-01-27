774b3d18a9b7917ab7561d03a59d0274ab96a2ff
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-01-27 16:32:11 +0900

    [Silver III] Title: 교수가 된 현우, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,29 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, n, cnt_5;
+int t, n;
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  cin >> T;
 
-  for (int t = 0; t < T; t++) {
+  cin >> t;
+
+  while (t--) {
     cin >> n;
 
-    cnt_5 = 0;
+    int ret = 0;
 
     for (int i = 5; i <= n; i *= 5) {
-      cnt_5 += n / i;
+      ret += n / i;
     }
-    cout << cnt_5 << '\n';
+    cout << ret << '\n';
   }
 
   return 0;
 }
-
-// 최대범위 10억 브루트포스 x
-// 결국 0의 개수는 10의 개수
-// 10의 개수는 2와 5의 개수
-// n의 승수의 개수
