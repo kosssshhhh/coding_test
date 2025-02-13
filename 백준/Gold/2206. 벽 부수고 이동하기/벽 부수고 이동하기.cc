bcb550c6c75de1750b48550de4b84feadfb4f2f2
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-02-13 16:11:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 132 ms, Memory: 15552 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -3,38 +3,34 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, a[1004][1004], visited[1004][1004][2];
+int n, m, a[1004][1004], dist[1004][1004][2];
+
 struct Item {
   int y;
   int x;
-  int is_wall_broken;
+  int is_wall_broke;
 };
 
 int main() {
   cin >> n >> m;
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      scanf("%1d", &a[i][j]);
-    }
+    for (int j = 0; j < m; j++) scanf("%1d", &a[i][j]);
   }
 
-  // BFS N, M, 벽 깨짐 여부
   queue<Item> q;
-  visited[0][0][0] = 1;
-  q.push({0, 0, 0});
 
-  while (q.size()) {
+  q.push({0, 0, 0});
+  dist[0][0][0] = 1;
+
+  while (!q.empty()) {
     int y = q.front().y;
     int x = q.front().x;
-    int is_wall_broken = q.front().is_wall_broken;
-    // cout << "y: " << y << " x: " << x << " is_wall_broken: " <<
-    // is_wall_broken
-    //      << '\n';
+    int is_wall_broke = q.front().is_wall_broke;
     q.pop();
 
     if (y == n - 1 && x == m - 1) {
-      cout << visited[y][x][is_wall_broken] << '\n';
+      cout << dist[y][x][is_wall_broke] << '\n';
       return 0;
     }
 
@@ -44,13 +40,16 @@ int main() {
 
       if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
 
-      // 벽이 없고, 방문하지 않은곳
-      if (a[ny][nx] == 0 && !visited[ny][nx][is_wall_broken]) {
-        visited[ny][nx][is_wall_broken] = visited[y][x][is_wall_broken] + 1;
-        q.push({ny, nx, is_wall_broken});
-      } else if (a[ny][nx] == 1 && !is_wall_broken && !visited[ny][nx][1]) {
-        // 벽이 있고, 벽을 부순적이 없고, 방문하지 않은곳
-        visited[ny][nx][1] = visited[y][x][0] + 1;
+      // 벽이 없고, 방문하지 않은 경우
+      if (!a[ny][nx] && !dist[ny][nx][is_wall_broke]) {
+        dist[ny][nx][is_wall_broke] = dist[y][x][is_wall_broke] + 1;
+
+        q.push({ny, nx, is_wall_broke});
+      }
+      // 벽이 있고, 벽을 부순적이 없고, 방문하지 않은 경우
+      else if (a[ny][nx] && !is_wall_broke && !dist[ny][nx][is_wall_broke]) {
+        dist[ny][nx][1] = dist[y][x][is_wall_broke] + 1;
+
         q.push({ny, nx, 1});
       }
     }
