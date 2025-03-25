943996351a6c2a5526da125902a1f1e7ed876afc
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-03-25 17:21:10 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -2,9 +2,8 @@ function solution(n)
 {
     let answer = 0;
     
-    String(n).split('').map(e => answer += +e);
-
-    return answer;
+    // String(n).split('').map(e => answer += +e);
+    return String(n).split('').reduce((acc, curr) => (+curr) + acc, 0) // 최종 형태
 }
 
 // n을 string으로 변환 후, split 메서드를 사용해 배열로 분해한 다음, 각 요소에 대해 다시 int 변환 후 더해준다.
