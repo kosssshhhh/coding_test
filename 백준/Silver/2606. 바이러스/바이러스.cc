437d840dcc5d79ddb68b7cb995dcedff47033fc6
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-04-08 17:14:14 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,34 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, a, b, visited[104];
+int n, m, from, to, visited[104];
 vector<int> adj[104];
 
 int dfs(int here) {
   visited[here] = 1;
-
   int ret = 0;
-  // cout << "here: " << here << '\n';
 
   for (int there : adj[here]) {
     if (visited[there]) continue;
 
-    ret += dfs(there) + 1;
+    ret += 1 + dfs(there);
   }
+
   return ret;
 }
 
 int main() {
   cin >> n >> m;
   for (int i = 0; i < m; i++) {
-    cin >> a >> b;
-    adj[a].push_back(b);
-    adj[b].push_back(a);
+    cin >> from >> to;
+    adj[from].push_back(to);
+    adj[to].push_back(from);
   }
 
   cout << dfs(1);
 
   return 0;
 }
-
-// dfs cnt
