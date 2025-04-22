21991bf1b163acf976898a9191a78a0efa77e6ad
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-04-22 10:24:20 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,32 +1,23 @@
-const dfs = (here, adj, visited) => {
-    visited.add(here)
-    
-    adj[here].forEach(there => {
-        if(!visited.has(there)){
-            dfs(there, adj, visited);
-        }
-    })
-}
+
 
 function solution(n, computers) {
     let ret = 0;
-    
-    const adj = Array.from({length: n}, () => []);
     const visited = new Set();
     
-    for (let i=0; i<computers.length; i++){
-        for (let j=0; j<computers[0].length; j++){
-            if(i !== j && computers[i][j]){
-                adj[i].push(j);
-                adj[j].push(i);
+    const dfs = (here) => {
+        visited.add(here);
+        
+        for (let i=0; i<n; i++){
+            if(computers[here][i] === 1 && !visited.has(i)){
+                dfs(i);
             }
         }
     }
     
-    for (let i=0; i<n; i++){
+    for(let i=0; i<n; i++){
         if(!visited.has(i)){
-            ret ++;
-            dfs(i, adj, visited);
+            ret++;
+            dfs(i);
         }
     }
     
