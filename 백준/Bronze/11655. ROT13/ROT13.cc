75ae5192e6b726bd03914a3f0dc06bf418da2e75
SeokHwan Ko <79795053+kosssshhhh@users.noreply.github.com>
2025-04-24 09:16:55 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+string s, ret;
 
 int main() {
   getline(cin, s);
 
   for (int i = 0; i < s.length(); i++) {
-    if (s[i] >= 'A' && s[i] <= 'Z') {
-      if (s[i] + 13 > 'Z') {
-        s[i] -= 13;
-      } else
-        s[i] += 13;
-    }
-
-    if (s[i] >= 'a' && s[i] <= 'z') {
-      if (s[i] + 13 > 'z') {
-        s[i] -= 13;
-      } else
-        s[i] += 13;
-    }
+    if (('a' <= s[i] && s[i] <= 'm') || ('A' <= s[i] && s[i] <= 'M'))
+      ret += s[i] + 13;
+    else if (('n' <= s[i] && s[i] <= 'z') || ('N' <= s[i] && s[i] <= 'Z'))
+      ret += s[i] - 13;
+    else
+      ret += s[i];
   }
-  cout << s << '\n';
+
+  cout << ret << '\n';
   return 0;
 }
