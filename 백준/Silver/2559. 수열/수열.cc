17d9d12962852317a21497c3d50548eb879a2501
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-02 13:51:29 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, ret = -1e9, psum[100004];
+const int INF = 1e9;
+int n, k, temp, ret = -INF, psum[100004];
 
 int main() {
-  cin >> n >> k;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
+  cin >> n >> k;
   for (int i = 1; i <= n; i++) {
-    int temp;
     cin >> temp;
     psum[i] = psum[i - 1] + temp;
   }
 
   for (int i = k; i <= n; i++) {
-    ret = max(ret, psum[i] - psum[i - k]);
+    int sum = psum[i] - psum[i - k];
+
+    ret = max(ret, sum);
   }
 
   cout << ret;
