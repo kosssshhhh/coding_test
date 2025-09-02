bfa49ff850e327b20bcb623a2df5ef69a81c6a2a
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-02 20:00:14 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -2,26 +2,26 @@
 using namespace std;
 
 int t, n;
-string temp1, temp2;
+string s1, s2;
 
 int main() {
   cin >> t;
   while (t--) {
+    cin >> n;
     map<string, int> mp;
     int ret = 1;
-    cin >> n;
+
     for (int i = 0; i < n; i++) {
-      cin >> temp1 >> temp2;
-      mp[temp2]++;
+      cin >> s1 >> s2;
+
+      mp[s2]++;
+    }
+    for (auto it : mp) {
+      ret *= it.second + 1;
     }
 
-    for (auto a : mp) {
-      ret *= a.second + 1;
-    }
     cout << ret - 1 << '\n';
   }
 
   return 0;
 }
-
-// map 활용한 조합
