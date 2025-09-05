c1ec0d3d6038445797c19e16b83d432ae00630eb
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-05 16:32:14 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[30], flag;
 string s, start, mid, last;
-int flag, cnt[30];
 
 int main() {
   cin >> s;
+
   for (int i = 0; i < s.size(); i++) {
-    cnt[s[i] - 'A']++;
+    arr[s[i] - 'A']++;
   }
 
   for (int i = 0; i < 30; i++) {
-    for (int j = 0; j < cnt[i] - 1; j += 2) {
+    for (int j = 0; j < arr[i] - 1; j += 2) {
       start += i + 'A';
       last = char(i + 'A') + last;
     }
 
-    if (cnt[i] & 1) {
+    if (arr[i] & 1) {
       flag++;
       if (flag > 1) {
         cout << "I'm Sorry Hansoo";
         return 0;
-      } else {
-        mid += i + 'A';
       }
+      mid += i + 'A';
     }
   }
 
-  cout << start + mid + last;
+  cout << start + mid << last;
 
   return 0;
 }
