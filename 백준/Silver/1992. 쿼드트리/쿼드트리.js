79901b11fdcfcb54d318c843957b95bec36a1901
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-10 13:56:53 +0900

    [Silver I] Title: 쿼드트리, Time: 92 ms, Memory: 9548 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.js
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.js
@@ -1,38 +1,32 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-// const input = fs.readFileSync('./input.txt').toString().split('\n');
+const input = require('fs').readFileSync(process.platform === "linux" ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
-const n = Number(input[0]);
-const map = input.slice(1).map(line => line.split(''));
+const n = input[0];
+const a = input.slice(1).map(line => line.split(''));
 
-const check = (y, x, size, map) => {
-    for (let i=y; i<y+size; i++){
-        for (let j=x; j<x+size; j++){
-            if(map[y][x] !== map[i][j]) return false;
+const check = (y, x, len) => {
+    const start = a[y][x];
+    
+    for (let i = y; i < y + len; i++){
+        for (let j = x; j< x + len; j++){
+            if(start !== a[i][j]) return false;
         }
     }
+
     return true;
 }
 
-const quad = (y, x, size, map) => {
-    let ret = "";
-    
-    if(n === 1 || check(y, x, size, map)){
-        ret += map[y][x];
-    }else{
-        ret += "(";
-        ret += quad(y, x, size/2, map);
-        ret += quad(y, x+size/2, size/2, map);
-        ret += quad(y+size/2, x, size/2, map);
-        ret += quad(y+size/2, x+size/2, size/2, map);
-        ret += ")";
+const go = (y, x, len) => {
+    if(len === 1) return a[y][x];
+
+    if(!check(y, x, len)){
+        return '(' + go(y, x, len / 2) + go(y, x + len / 2, len / 2) + go(y + len / 2, x, len / 2) + go(y + len / 2, x + len / 2, len / 2) + ')';
     }
-
-    return ret;
+    
+    return a[y][x];
 }
 
-const answer = (n, map) => {
-    console.log(quad(0, 0, n, map));
+function answer(n){
+    return go(0, 0, n);
 }
 
-answer(n, map);
+console.log(answer(n));
