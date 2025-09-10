75652aca305a709ab3c6b79afcefeab7786b4795
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-10 18:57:53 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,37 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, j, ret;
-int a;
+int N, M, J, a, l, r, ret;
 
 int main() {
-  cin >> n >> m;
-  cin >> j;
+  cin >> N >> M >> J;
 
-  int l = 1;
-  int r = m;
+  l = 1, r = l + M - 1;
 
-  for (int i = 0; i < j; i++) {
+  for (int i = 0; i < J; i++) {
     cin >> a;
-    if (a > r) {
-      while (a != r) {
-        l++;
-        r++;
-        ret++;
-      }
-    }
-    if (a < l) {
-      while (a != l) {
-        l--;
-        r--;
-        ret++;
-      }
-    }
-    if (a <= r && a >= l) {
+
+    if (r < a) {
+      ret += a - r;
+      l += a - r;
+      r = a;
+
+    } else if (a < l) {
+      ret += l - a;
+      r -= l - a;
+      l = a;
+    } else {
       continue;
     }
   }
-
   cout << ret;
 
   return 0;
