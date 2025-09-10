69cfa16ceddf98c4dd366bf62aabb1aceeb18214
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-10 19:05:24 +0900

    [Silver V] Title: 사과 담기 게임, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.js
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.js
@@ -1,39 +1,25 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-// const input = fs.readFileSync('./input.txt').toString().split('\n');
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const apples = input.slice(2).map(Number);
 
-const [n, m] = input[0].split(' ').map(a => Number(a));
-const apples = input.slice(2).filter(a => a !== '').map(Number);
+function answer (N, M, apples){
+    let l = 1, r = l + M - 1, ret = 0;
 
-const answer = (n, m, apples) => {
-    let ret = 0;
-
-    const j = apples.length;
-    let l = 1;
-    let r = l + m - 1;
-    
     apples.forEach(apple => {
-        // console.log(apple);
-        if(apple >= l && apple <= r){
-            // 이동 없음
-        }
-        else if (apple < l) {
-            const move = l - apple;
-            l -= move;
-            r -= move;
-            ret += move;
-            // console.log(`왼쪽으로 ${move}칸 이동`);
-        } else if (apple > r) {
-            const move = apple - r;
-            l += move;
-            r += move;
-            ret += move;
-            // console.log(`오른쪽으로 ${move}칸 이동`);
+        if(r < apple){
+            const dist = apple - r;
+            r = apple;
+            l += dist;
+            ret += dist;
+        }else if(apple < l){
+            const dist = l - apple;
+            l = apple;
+            r -= dist;
+            ret += dist;
         }
     })
+
     return ret;
 }
 
-console.log(answer(n, m, apples));
-
-// js 백준에서 입출력의 중요성에 대해서 알게됨
+console.log(answer(N, M, apples));
