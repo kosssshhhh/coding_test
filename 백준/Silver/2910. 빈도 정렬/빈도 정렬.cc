1e9c6bafd0128b5596c367ab0024a444678cfd4b
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-10 19:20:39 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, c, a;
-map<int, int> cnt;
-map<int, int> first;
-
+int N, C, a;
 vector<int> v;
-bool cmp(int a, int b) {
-  if (cnt[a] == cnt[b]) return first[a] < first[b];
+map<int, int> cnt, idx;
+
+bool comp(int a, int b) {
+  if (cnt[a] == cnt[b]) {
+    return idx[a] < idx[b];
+  }
   return cnt[a] > cnt[b];
 }
 
 int main() {
-  cin >> n >> c;
+  cin >> N >> C;
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < N; i++) {
     cin >> a;
-    cnt[a]++;
-    if (!first[a]) first[a] = i + 1;
     v.push_back(a);
+
+    cnt[a]++;
+    if (!idx[a]) {
+      idx[a] = i + 1;
+    }
   }
+  sort(v.begin(), v.end(), comp);
 
-  sort(v.begin(), v.end(), cmp);
-
-  for (int a : v) cout << a << ' ';
+  for (int a : v) {
+    cout << a << ' ';
+  }
 
   return 0;
 }
-
-// 빈도 정렬
-// 등장 횟수, 먼저 나온것
-// 두개의 자료구조에 각각 저장?
