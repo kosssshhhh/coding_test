6d5ca5845d29f1da190005fe7d7c2e6750ca668d
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-11 14:40:29 +0900

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -1,52 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int m;
-string s, st;
+int n;
+string s;
 vector<string> v;
+
+string go(string s) {
+  while (true) {
+    if (s.front() == '0')
+      s.erase(s.begin());
+    else
+      break;
+  }
+  if (s.size() == 0) return "0";
+  return s;
+}
 
 bool cmp(string a, string b) {
   if (a.size() == b.size()) return a < b;
   return a.size() < b.size();
 }
 
-void go() {
-  while (true) {
-    if (st.size() && st.front() == '0')
-      st.erase(st.begin());
-    else
-      break;
-  }
-  if (st.size() == 0) st = "0";
-  v.push_back(st);
-  return;
-}
-
 int main() {
-  cin >> m;
-  for (int i = 0; i < m; i++) {
+  cin >> n;
+
+  while (n--) {
     cin >> s;
-    st = "";
-    for (int j = 0; j < s.size(); j++) {
-      if (s[j] >= '0' && s[j] <= '9') {
-        st += s[j];
-      } else {
-        if (st != "") {
-          go();
-          st = "";
-        }
+    string str = "";
+
+    for (char c : s) {
+      if (c < 65) {
+        str += c;
+      } else if (str.size()) {
+        v.push_back(go(str));
+        str = "";
       }
     }
-    if (st != "") {
-      go();
-      st = "";
-    }
+    if (str.size()) v.push_back(go(str));
   }
 
   sort(v.begin(), v.end(), cmp);
 
-  for (string ret : v) {
-    cout << ret << '\n';
+  for (string s : v) {
+    cout << s << '\n';
   }
 
   return 0;
