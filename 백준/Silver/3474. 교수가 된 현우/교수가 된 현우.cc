1373c4f8774963f562229ed4376cbac846ca3b6a
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-12 15:28:05 +0900

    [Silver III] Title: 교수가 된 현우, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -7,17 +7,15 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-
   cin >> t;
-
   while (t--) {
     cin >> n;
-
     int ret = 0;
 
     for (int i = 5; i <= n; i *= 5) {
       ret += n / i;
     }
+
     cout << ret << '\n';
   }
 
