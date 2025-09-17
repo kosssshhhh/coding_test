a1f0c511aa3e08261f0f23f731878d3d53992047
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-17 13:07:50 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,49 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, ret, root, visited[50];
+int n, m, root, ret;
 vector<int> adj[50];
 
 void go(int u) {
   if (u == m) return;
-
-  int children = 0;
-
+  int cnt = 0;
   for (int v : adj[u]) {
     if (v == m) continue;
-    children++;
     go(v);
+    cnt++;
   }
 
-  if (children == 0) ret++;
+  if (cnt == 0) ret++;
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
   cin >> n;
-
   for (int i = 0; i < n; i++) {
     int p;
     cin >> p;
+
     if (p == -1) {
       root = i;
     } else {
       adj[p].push_back(i);
     }
   }
-
   cin >> m;
-
-  if (m == root) {
-    cout << 0 << '\n';
-    return 0;
-  }
 
   go(root);
 
-  cout << ret << '\n';
+  cout << ret;
 
   return 0;
 }
