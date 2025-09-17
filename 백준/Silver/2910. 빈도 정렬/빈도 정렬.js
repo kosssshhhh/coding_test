b56e0df5c9d27fc1432643b7e1c7739622764625
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-17 11:40:36 +0900

    [Silver III] Title: 빈도 정렬, Time: 148 ms, Memory: 10032 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.js
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.js
@@ -1,40 +1,27 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-// const input = fs.readFileSync('./input.txt').toString().split('\n');
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
-const [n, c] = input[0].split(' ').map(a => Number(a));
-const arr = input[1].split(' ').map(a => Number(a));
+const [N, C] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 
-const answer = (n, c, arr) => {
-    const map1 = new Map(); // 등장 횟수
-    const map2 = new Map(); // 등장 순서
+const answer = () => {
+    const cnt = Object.create(null);
+    const idx = Object.create(null);
 
-    arr.forEach((a, i) => (map1[a] = 0));
     arr.forEach((a, i) => {
-
-        map1[a]++;
-        
-        if(map2[a]){
-            // nothing
-        }else {
-            map2[a] = i+1;
+        cnt[a]++;
+        if(cnt[a]) cnt[a]++;
+        else cnt[a] = 1;
+        if(!idx[a]) idx[a] = i + 1;
+    })
+    
+    arr.sort((a, b) => {
+        if(cnt[a] === cnt[b]){
+            return idx[a] - idx[b];
         }
+        return cnt[b] - cnt[a];
     })
 
-     arr.sort((a, b) => {
-        if(map1[a] === map1[b]){
-            return map2[a] - map2[b];
-        }
-        return map1[b] - map1[a];
-    })
-    let  ret = "";
-    arr.forEach(a => {
-        ret += a;
-        ret += ' ';
-    });
-
-    console.log(ret);
-
+    console.log(arr.join(' '));
 }
 
-answer(n, c, arr);
+answer();
