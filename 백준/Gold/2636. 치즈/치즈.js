4eecb419ccb7672e6631c843fb3ccdb02da0a792
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-20 15:16:55 +0900

    [Gold IV] Title: 치즈, Time: 176 ms, Memory: 13092 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.js
+++ 백준/Gold/2636. 치즈/치즈.js
@@ -1,56 +1,63 @@
-const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split('\n');
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
-const a = input.slice(1).map(line => line.split(' ').map(Number));
+const arr = input.slice(1).map(line => line.split(' ').map(Number));
 
 const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let cnt = 0, ret = 0;
-let visited = Array.from({length: N}, () => Array(M).fill(0));
+let visited = Array.from({length: 104}, () => Array(104).fill(0));
 let v = [];
+
+
+let cnt = 0, t = 0;
+arr.forEach(line => line.forEach(c => c === 1 && cnt++));
 
 const dfs = (y, x) => {
     visited[y][x] = 1;
-
-    for (let i = 0; i < 4; i++){
-        const [ny, nx] = [y + dir[i][0], x + dir[i][1]];
+    
+    for (const [dy, dx] of dir){
+        const ny = y + dy;
+        const nx = x + dx;
         
         if(ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx]) continue;
-        if(a[ny][nx] == 1){
+        if(arr[ny][nx] === 1) {
             v.push([ny, nx]);
             visited[ny][nx] = 1;
-        }else{
+        }else if(arr[ny][nx] == 0){
             dfs(ny, nx);
         }
     }
 }
 
+
 const answer = () => {
-    a.forEach(line => line.forEach(i => i === 1 && cnt++))
-    
-    while(cnt > 0){
-        ret++;
+    while(cnt > v.length){
+        t++;
 
-        for(let i = 0; i < N; i++){
+        for (let i = 0; i < N; i++){
             dfs(i, 0);
-            dfs(i, M - 1);
+            dfs(i, M);
         }
-
+    
         for (let i = 0; i < M; i++){
             dfs(0, i);
-            dfs(N - 1, i);
+            dfs(N, i);
         }
 
         if(cnt - v.length <= 0) break;
         cnt -= v.length;
+        
+        for (let i = 0; i < v.length; i++){
+            arr[v[i][0]][v[i][1]] = 0; // 업데이트
+        }
 
-        v.forEach(([y, x]) => a[y][x] = 0);
-
-        visited = Array.from({length: N}, () => Array(M).fill(0));
-        v = [];
+        visited = Array.from({length: 104}, () => Array(104).fill(0));
+        v = []
+        
     }
 
-    console.log(ret);
+    console.log(t);
     console.log(cnt);
+    
 }
 
 answer();
