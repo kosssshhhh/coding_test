5c7e784ba03b6964bb089dec3cc32faacf2705e2
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-30 15:04:39 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -3,11 +3,10 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int m, n, cnt, ret = 100, wall_cnt;
-int a[10][10], visited[10][10];
+int N, M, wall_cnt, cnt, ret = 1e9, a[10][10], visited[10][10];
 vector<pair<int, int>> v;
 
-void dfs(int y, int x) {
+void go(int y, int x) {
   visited[y][x] = 1;
   cnt++;
 
@@ -15,22 +14,22 @@ void dfs(int y, int x) {
     int ny = y + dy[i];
     int nx = x + dx[i];
 
-    if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
-    if (a[ny][nx] == 0 && visited[ny][nx] == 0) {
-      dfs(ny, nx);
-    }
+    if (ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx]) continue;
+    if (a[ny][nx] == 0) go(ny, nx);
   }
 }
 
 int main() {
-  cin >> m >> n;
+  cin >> N >> M;
 
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
       cin >> a[i][j];
-
-      if (a[i][j] == 0) v.push_back({i, j});
-      if (a[i][j] == 1) wall_cnt++;
+      if (a[i][j] == 0) {
+        v.push_back({i, j});
+      } else if (a[i][j] == 1) {
+        wall_cnt++;
+      }
     }
   }
 
@@ -41,23 +40,23 @@ int main() {
         a[v[j].first][v[j].second] = 1;
         a[v[k].first][v[k].second] = 1;
 
-        for (int l = 0; l < m; l++) {
-          for (int o = 0; o < n; o++) {
-            if (a[l][o] == 2 && visited[l][o] == 0) {
-              dfs(l, o);
-            }
+        for (int l = 0; l < N; l++) {
+          for (int m = 0; m < M; m++) {
+            if (a[l][m] == 2 && !visited[l][m]) go(l, m);
           }
         }
-        ret = min(cnt, ret);
+
+        ret = min(ret, cnt);
         cnt = 0;
         memset(visited, 0, sizeof(visited));
+
         a[v[i].first][v[i].second] = 0;
         a[v[j].first][v[j].second] = 0;
         a[v[k].first][v[k].second] = 0;
       }
     }
   }
-  cout << m * n - ret - wall_cnt - 3 << '\n';
 
+  cout << M * N - wall_cnt - 3 - ret;
   return 0;
 }
