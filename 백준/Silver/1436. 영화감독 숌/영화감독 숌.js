f17c44e29b95ab7d31ff5fb6011cc6a39d43203d
SeokHwan Ko <kosukwhon@gmail.com>
2025-09-30 16:28:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 380 ms, Memory: 23252 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -1,18 +1,20 @@
-const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
-const answer = (n) => {
+const N = Number(input[0]);
+
+const answer = () => {
     let num = 666, ret = 0;
-
+    
     while(true){
-        if(num.toString().includes("666")){
+        if(num.toString().includes('666')){
             ret++;
-            if(ret === n) {
-                return num;
+            if(ret === N){
+                console.log(num);
+                return;
             }
         }
         num++;
     }
-    
 }
 
-console.log(answer(Number(input)))
+answer();
