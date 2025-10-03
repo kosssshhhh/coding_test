272f4e7da1ea66d5011c81df4119dc9acdcd2690
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-03 18:43:15 +0900

    [Gold V] Title: 보물섬, Time: 96 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -3,60 +3,60 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int m, n;
-string temp;
-char a[54][54];
-queue<pair<int, int>> q;
-int visited[54][54];
-int mx = 0, ret = 0;
-
-void bfs(int y, int x) {
-  mx = 0;
-  visited[y][x] = 1;
-  q.push({y, x});
-  while (q.size()) {
-    tie(y, x) = q.front();
-    q.pop();
-
-    for (int i = 0; i < 4; i++) {
-      int ny = y + dy[i];
-      int nx = x + dx[i];
-      if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
-      if (visited[ny][nx] || a[ny][nx] == 'W') continue;
-      visited[ny][nx] = visited[y][x] + 1;
-
-      mx = max(mx, visited[ny][nx]);
-
-      q.push({ny, nx});
-    }
-  }
-}
+int n, m, y, x, ret = -1;
+int a[54][54], visited[54][54];
+vector<pair<int, int>> v;
 
 int main() {
-  cin >> m >> n;
-  for (int i = 0; i < m; i++) {
+  cin >> n >> m;
+
+  for (int i = 0; i < n; i++) {
+    string temp;
     cin >> temp;
-    for (int j = 0; j < n; j++) {
-      a[i][j] = temp[j];
+    for (int j = 0; j < m; j++) {
+      if (temp[j] == 'L') {
+        a[i][j] = 1;
+        v.push_back({i, j});
+      } else {
+        a[i][j] = 0;
+      }
     }
+  }
+  for (int t = 0; t < v.size(); t++) {
+    int mx = -1;
+    memset(visited, 0, sizeof(visited));
+
+    queue<pair<int, int>> q;
+    visited[v[t].first][v[t].second] = 1;
+    q.push(v[t]);
+
+    while (q.size()) {
+      tie(y, x) = q.front();
+      q.pop();
+
+      for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+        if (a[ny][nx]) {
+          q.push({ny, nx});
+          visited[ny][nx] = visited[y][x] + 1;
+        }
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        mx = max(visited[i][j], mx);
+        // cout << visited[i][j] << ' ';
+      }
+      //   cout << '\n';
+    }
+
+    ret = max(ret, mx);
   }
 
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      memset(visited, 0, sizeof(visited));
-      while (q.size()) {
-        q.pop();
-      }
-      if (a[i][j] == 'L') {
-        bfs(i, j);
-        ret = max(mx, ret);
-      };
-    }
-  }
   cout << ret - 1;
   return 0;
 }
-
-// 모든 지점에서 최단 거리 구하기, BFS
-// BFS의 처음 지점 1로 바꿔주지 않아서 55% 지점에서 실패
-// 따라서 첫 지점을 방문했을때 1로 초기화 해준 후, 결과 ret에서 -1 하는 방식
