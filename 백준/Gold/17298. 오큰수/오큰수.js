4c36857a3cf48ea16640194cc20d654b5dc372c9
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-05 19:07:49 +0900

    [Gold IV] Title: 오큰수, Time: 736 ms, Memory: 205728 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -1,21 +1,22 @@
 const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
-const n = Number(input[0]);
-const numbers = input[1].split(' ').map(Number);
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
 
 const answer = () => {
-    const stk = [];
-    const ret = Array(n).fill(-1);
-    
-    for (let i = 0; i < n; i++){
-        while(stk.length > 0 && numbers[stk[stk.length - 1]] < numbers[i]){
-            ret[stk[stk.length - 1]] = numbers[i];
-            stk.pop();
-        }
-        stk.push(i);
-    }
+  const stk = [];
+  const ret = Array(N).fill(-1);
 
-    console.log(ret.join(' '));
+  for (let i = 0; i < N; i++){
+    while(stk.length > 0 && arr[stk[stk.length - 1]] < arr[i]){
+      ret[stk[stk.length - 1]] = arr[i];
+      stk.pop();
+    }
+    stk.push(i);
+  }
+
+  console.log(ret.join(' '));
+  
 }
 
 answer();
