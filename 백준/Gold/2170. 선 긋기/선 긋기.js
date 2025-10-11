b2725ca4c04badbf13a56b83419d2631e4b73677
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-11 11:19:09 +0900

    [Gold V] Title: 선 긋기, Time: 2508 ms, Memory: 244784 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.js
+++ 백준/Gold/2170. 선 긋기/선 긋기.js
@@ -1,23 +1,27 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt').toString().trim().split('\n');
 
-const arr = input.slice(1).map(e => e.split(' ').map(e => Number(e)));
+const N = Number(input[0]);
+const arr = input.slice(1).map(line => line.split(' ').map(Number));
 
-const answer = (arr) => {
-    let ret = 0;
-    arr.sort((a, b) => a[0] - b[0]);
-    let [l, r] = arr[0];
-    arr = arr.slice(1);
-    
-    for (let [start, end] of arr){
-        if(start <= r && r < end) r = end;
-        else if(r < start){
-            ret += r - l;
-            l = start, r = end;
-        }
-    }
-    ret += r - l;
-    return ret;
+arr.sort((a, b) => {
+  return a[0] - b[0];
+})
+
+let from = arr[0][0], to = arr[0][1];
+let ret = 0;
+
+for (let i = 1; i < N; i++){
+  let [s, e] = arr[i];
+  
+  if(s < to && to < e){
+    to = e;
+  }else if(to <= s){
+    ret += to - from;
+    from = s;
+    to = e;
+  }
 }
 
-console.log(answer(arr));
+ret += to - from;
+
+console.log(ret);
