5dec9dbd7240b7f92499059516b76b978b2bc225
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-12 19:11:23 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 2952 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,41 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[3], visited[64][64][64];
-int _a[6][3] = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1},
-                {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
-
 struct A {
   int a, b, c;
 };
+
+int attacks[6][3] = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1},
+                     {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
+
+int n, scv[3];
+int visited[62][62][62];
 queue<A> q;
 
-int solve(int a, int b, int c) {
-  visited[a][b][c] = 1;
-  q.push({a, b, c});
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> scv[i];
+  }
+
+  visited[scv[0]][scv[1]][scv[2]] = 1;
+  q.push({scv[0], scv[1], scv[2]});
+
   while (q.size()) {
     int a = q.front().a;
     int b = q.front().b;
     int c = q.front().c;
     q.pop();
+    if (a == 0 && b == 0 && c == 0) continue;
 
     for (int i = 0; i < 6; i++) {
-      int na = max(0, a - _a[i][0]);
-      int nb = max(0, b - _a[i][1]);
-      int nc = max(0, c - _a[i][2]);
+      int na = max(0, a - attacks[i][0]);
+      int nb = max(0, b - attacks[i][1]);
+      int nc = max(0, c - attacks[i][2]);
 
       if (visited[na][nb][nc]) continue;
+
       visited[na][nb][nc] = visited[a][b][c] + 1;
       q.push({na, nb, nc});
     }
   }
-  return visited[0][0][0] - 1;
-}
 
-int main() {
-  cin >> n;
-  for (int i = 0; i < n; i++) cin >> a[i];
-  cout << solve(a[0], a[1], a[2]) << '\n';
+  cout << visited[0][0][0] - 1 << '\n';
 
   return 0;
 }
