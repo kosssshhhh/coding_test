bc8cd4d901c31f8be782c5c27cbc4c725cd3c101
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-14 16:46:43 +0900

    [Gold II] Title: 게임, Time: 160 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.js
+++ 백준/Gold/1103. 게임/게임.js
@@ -5,33 +5,31 @@ const a = input.slice(1).map(line => line.split(''));
 
 const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 const visited = Array.from({length: n}, () => Array(m).fill(0));
-const dp = Array.from({length: n}, () => Array(m).fill(0));
+const dp = Array.from({length: n}, () => Array(m).fill(-1));
 
-function go(y, x){
+function go (y, x) {
   if(y < 0 || x < 0 || y >= n || x >= m || a[y][x] === 'H') return 0;
-
-  if(visited[y][x]) {
+  if(visited[y][x]){
     console.log(-1);
     process.exit(0);
   }
 
-  if(dp[y][x]) return dp[y][x];
-
+  if(dp[y][x] !== -1) return dp[y][x];
   let ret = 0;
+
   visited[y][x] = 1;
+  for (const [dy, dx] of dir)  {
+    let ny = y + dy * a[y][x], nx = x + dx * a[y][x];
 
-  for (const [dy, dx] of dir){
-    let ny = y + dy * a[y][x];
-    let nx = x + dx * a[y][x];
-
-    ret = Math.max(dp[y][x], go(ny, nx) + 1);
+    ret = Math.max(go(ny, nx) + 1, dp[y][x]);
     dp[y][x] = ret;
   }
   visited[y][x] = 0;
+
   return ret;
 }
 
-function answer(){
+function answer() {
   console.log(go(0, 0));
 }
 
