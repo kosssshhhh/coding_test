06c3feb022718dde48feeb515a506b7cae734d0c
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-15 10:37:56 +0900

    [Gold III] Title: 불!, Time: 532 ms, Memory: 68548 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.js
+++ 백준/Gold/4179. 불！/불！.js
@@ -3,73 +3,68 @@ const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/st
 const [R, C] = input[0].split(' ').map(Number);
 const a = input.slice(1).map(line => line.split(''));
 
+const f_visited = Array.from({length: R}, () => Array(C).fill(Infinity));
+const j_visited = Array.from({length: R}, () => Array(C).fill(0));
+
 const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+let ret = 0;
 
 const queue = [];
+const fire = [];
 let jh;
-const fire_visited = Array.from({length: R}, () => Array(C).fill(Infinity));
-const jihun_visited = Array.from({length: R}, () => Array(C).fill(Infinity));
 
-for (let i = 0; i<R; i++){
+for (let i = 0; i< R; i++){
   for (let j = 0; j<C; j++){
-    if(a[i][j] === 'F') {
-      fire_visited[i][j] = 1;
+    if(a[i][j] === 'F'){
       queue.push([i, j]);
+      f_visited[i][j] = 1;
     }else if(a[i][j] === 'J'){
-      jihun_visited[i][j] = 1;
       jh = [i, j];
     }
   }
 }
 
-
-
 function answer() {
-  // 불 이동
-  while(queue.length){
+  while(queue.length) {
     const [y, x] = queue.shift();
     
     for (const [dy, dx] of dir){
       let ny = y + dy, nx = x + dx;
-      
-      if(ny < 0 || nx < 0 || ny >= R || nx >= C || fire_visited[ny][nx] !== Infinity || a[ny][nx] === '#') continue;
-      if(a[ny][nx] === '.' || a[ny][nx] === 'J'){
-        fire_visited[ny][nx] = fire_visited[y][x] + 1;
-        queue.push([ny, nx]);
-      }
-    }
+
+      if(ny < 0 || nx < 0 || ny >=R || nx >= C || f_visited[ny][nx] !== Infinity || a[ny][nx] === '#') continue;
+      f_visited[ny][nx] = f_visited[y][x] + 1; 
+      queue.push([ny, nx]);
+    } 
   }
 
-  // 지훈 이동
   queue.push(jh);
-  jihun_visited[jh[0]][jh[1]] = 1;
+  j_visited[jh[0]][jh[1]] = 1;
+  
   while(queue.length){
     const [y, x] = queue.shift();
 
-    if(y === 0 || x === 0 || y === R - 1 || x === C - 1){
-      console.log(jihun_visited[y][x]);
-      return;
+    if(y === 0 || x === 0 || y === R-1 || x === C-1){
+      ret = j_visited[y][x];
+      break;
     }
 
     for (const [dy, dx] of dir){
       let ny = y + dy, nx = x + dx;
 
-      if(ny < 0 || nx < 0 || ny >= R || nx >= C || jihun_visited[ny][nx] !== Infinity || a[ny][nx] === '#' || a[ny][nx] === 'F') continue;
-      if(fire_visited[ny][nx] <= jihun_visited[y][x] + 1) continue;
-      
+      if(ny < 0 || nx < 0 || ny >=R || nx >= C || j_visited[ny][nx] || a[ny][nx] === '#') continue;
+      if(f_visited[ny][nx] <= j_visited[y][x] + 1) continue;
+
+      j_visited[ny][nx] = j_visited[y][x] + 1;
       queue.push([ny, nx]);
-      jihun_visited[ny][nx] = jihun_visited[y][x] + 1;
     }
   }
   
-  
-  console.log("IMPOSSIBLE")
+  console.log(ret ? ret : 'IMPOSSIBLE');
+
 }
 
 answer();
 
-
-// BFS
-// 불 먼저 BFS -> 지훈 BFS
-// 이동거리가 지훈이가 더 작아야만 이동 가능
-// 
+// 불, 지훈 위치 확인
+// 불 먼저 BFS
+// 지훈 BFS하면서 불보다 작다는 조건
