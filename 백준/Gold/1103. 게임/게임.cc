f225fa976edef275b2b796880567c436c6b4be66
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-17 15:56:30 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -3,26 +3,29 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-string temp;
-int n, m, visited[54][54], dp[54][54];
-char board[54][54];
+int n, m;
+int visited[54][54], dp[54][54];
+char a[54][54];
 
 int go(int y, int x) {
-  if (y < 0 || x < 0 || y >= n || x >= m || board[y][x] == 'H') return 0;
+  if (y < 0 || x < 0 || y >= n || x >= m || a[y][x] == 'H') return 0;
   if (visited[y][x]) {
-    cout << "-1" << '\n';
+    cout << -1 << '\n';
     exit(0);
   }
 
-  int &ret = dp[y][x];
+  int& ret = dp[y][x];
   if (ret) return ret;
+
   visited[y][x] = 1;
 
   for (int i = 0; i < 4; i++) {
-    int ny = y + dy[i] * (board[y][x] - '0');
-    int nx = x + dx[i] * (board[y][x] - '0');
-    ret = max(go(ny, nx) + 1, ret);
+    int ny = y + dy[i] * (a[y][x] - '0');
+    int nx = x + dx[i] * (a[y][x] - '0');
+
+    ret = max(ret, go(ny, nx) + 1);
   }
+
   visited[y][x] = 0;
   return ret;
 }
@@ -30,13 +33,14 @@ int go(int y, int x) {
 int main() {
   cin >> n >> m;
   for (int i = 0; i < n; i++) {
+    string temp;
     cin >> temp;
     for (int j = 0; j < m; j++) {
-      board[i][j] = temp[j];
+      a[i][j] = temp[j];
     }
   }
 
-  cout << go(0, 0);
+  cout << go(0, 0) << '\n';
 
   return 0;
 }
