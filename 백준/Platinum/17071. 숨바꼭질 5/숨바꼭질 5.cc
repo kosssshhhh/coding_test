1e0d77dd8334098afcb3d19cc40f7aa4591a4cd1
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-20 13:06:55 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -2,37 +2,39 @@
 using namespace std;
 
 const int max_n = 500000;
-int visited[2][max_n + 4], a, b, ok, turn = 1;
+int n, k, turn = 1, ok, visited[2][max_n + 4];
 
 int main() {
-  cin >> a >> b;
+  cin >> n >> k;
 
-  if (a == b) {
+  if (n == k) {
     cout << 0 << '\n';
     return 0;
   }
 
   queue<int> q;
-  visited[0][a] = 1;
-  q.push(a);
+  q.push(n);
+  visited[0][n] = 1;
 
   while (q.size()) {
-    b += turn;
-    if (b > max_n) break;
-    if (visited[turn % 2][b]) {
+    k += turn;
+
+    if (k > max_n) break;
+    if (visited[turn % 2][k]) {
       ok = true;
       break;
     }
+
     int qSize = q.size();
     for (int i = 0; i < qSize; i++) {
       int x = q.front();
       q.pop();
 
-      for (int nx : {x + 1, x - 1, x * 2}) {
+      for (int nx : {x - 1, x + 1, x * 2}) {
         if (nx < 0 || nx > max_n || visited[turn % 2][nx]) continue;
         visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
 
-        if (nx == b) {
+        if (nx == k) {
           ok = 1;
           break;
         }
@@ -52,5 +54,5 @@ int main() {
   return 0;
 }
 
-// flood fill: BFS에서 같은 레벨에 있는 값에 같은 조건을 적용하고 싶을 때
-// 수빈이가 홀짝인지 체크: 동생이 수빈이가 도착했던 포인트에 이후에 도착해도 -1, 1 반복하며 동생과 만날 수 있음.
+// 50만 -> 2차원 배열로 만들수 없음.
+// 홀 짝 상태로만 다시 만날 수 있는지 확인 가능
