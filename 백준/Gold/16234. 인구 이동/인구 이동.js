d5e6b6c6e59ac4bb92e48d6bdcb242c6c0d81129
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-28 12:53:54 +0900

    [Gold IV] Title: 인구 이동, Time: 988 ms, Memory: 27896 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.js
+++ 백준/Gold/16234. 인구 이동/인구 이동.js
@@ -4,65 +4,53 @@ const [N, L, R] = input[0].split(' ').map(Number);
 const a = input.slice(1).map(line => line.split(' ').map(Number));
 
 const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let visited = Array.from({length: N}, () => Array(N).fill(0));
-let v = [];
-let ret = 0;
+let v = [], ret = 0;
+let visited = Array.from({length: 54}, () => Array(54).fill(0));
 
 function dfs(y, x) {
-  visited[y][x] = 1; 
-  let ret = a[y][x];
-  v.push([y, x]);
-  
-  for (const [dy, dx] of dir){
-    let ny = y + dy;
-    let nx = x + dx;
+    visited[y][x] = 1;
+    v.push([y, x]);
+    let ret = a[y][x];
 
-    if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
-    const dist = Math.abs(a[ny][nx] - a[y][x]);
+    for (let d of dir){
+        const [ny, nx] = [y + d[0], x + d[1]];
 
-    if(L <= dist && dist <= R){
-      ret += dfs(ny, nx);
+        if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
+        let dist = Math.abs(a[ny][nx] - a[y][x]);
+        
+        if(L <= dist && dist <= R){
+            ret += dfs(ny, nx);
+        }
     }
-  }
-
-  return ret;
+    return ret;
 }
 
-function answer() {
-  while(true){
-    let flag = false;
-    visited = Array.from({length: N}, () => Array(N).fill(0));
+function answer(){
+    while(true){
+        visited = Array.from({length: 54}, () => Array(54).fill(0));
+        let flag = false;
 
-    for (let i = 0; i < N; i++){
-      for (let j = 0; j < N; j++){
-        if(visited[i][j]) continue;
+        for (let i = 0; i < N; i++){
+            for (let j = 0; j < N; j++){
+                if(visited[i][j]) continue;
+                v = [];
 
-        v = [];
+                let sum = dfs(i, j);
 
-        let sum = dfs(i, j);
-        if(v.length <= 1) continue;
-  
-        // 인구 이동
-        for(const [y, x] of v){
-          a[y][x] = Math.floor(sum / v.length);
+                if(v.length !== 1){
+                    flag = true;
+                    for(let it of v){
+                        a[it[0]][it[1]] = Math.floor(sum / v.length);
+                    }
+                }
+
+            }
         }
-        
-        flag = true;
-      }
+        if(flag) ret++;
+        else break;
     }
-
-    if(!flag) break;
-    ret++;
-  }
-
-  console.log(ret);
-  
-
+    console.log(ret);
+    
 }
 
 answer();
-
-
-// 두 나라의 인구 차이 L <= a <= R
-// connected component
-// 
