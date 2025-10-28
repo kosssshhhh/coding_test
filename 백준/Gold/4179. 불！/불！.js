64119ddf795a7e6c6f4b7e3d7070f284e16cce70
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-28 13:42:12 +0900

    [Gold III] Title: 불!, Time: 520 ms, Memory: 68616 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.js
+++ 백준/Gold/4179. 불！/불！.js
@@ -3,68 +3,60 @@ const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/st
 const [R, C] = input[0].split(' ').map(Number);
 const a = input.slice(1).map(line => line.split(''));
 
+const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 const f_visited = Array.from({length: R}, () => Array(C).fill(Infinity));
 const j_visited = Array.from({length: R}, () => Array(C).fill(0));
-
-const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let ret = 0;
+let ji;
 
 const queue = [];
-const fire = [];
-let jh;
 
-for (let i = 0; i< R; i++){
-  for (let j = 0; j<C; j++){
-    if(a[i][j] === 'F'){
-      queue.push([i, j]);
-      f_visited[i][j] = 1;
-    }else if(a[i][j] === 'J'){
-      jh = [i, j];
+for (let i = 0; i < R; i++){
+    for (let j = 0; j < C; j++){
+        if(a[i][j] === 'F'){
+            f_visited[i][j] = 1;
+            queue.push([i, j]);
+        }else if(a[i][j] === 'J'){
+            ji = [i, j];
+        }
     }
-  }
 }
 
-function answer() {
-  while(queue.length) {
-    const [y, x] = queue.shift();
+function answer(){
+    while(queue.length){
+        const [y, x] = queue.shift();
+        
+        for (const d of dir){
+            const [ny, nx] = [y + d[0], x + d[1]];
+
+            if(ny < 0 || nx < 0 || ny >= R || nx >= C || f_visited[ny][nx] !== Infinity || a[ny][nx] === '#') continue;
+            f_visited[ny][nx] = f_visited[y][x] + 1;
+            queue.push([ny, nx]);
+        }
+    }
     
-    for (const [dy, dx] of dir){
-      let ny = y + dy, nx = x + dx;
+    queue.push(ji);
+    j_visited[ji[0]][ji[1]] = 1;
 
-      if(ny < 0 || nx < 0 || ny >=R || nx >= C || f_visited[ny][nx] !== Infinity || a[ny][nx] === '#') continue;
-      f_visited[ny][nx] = f_visited[y][x] + 1; 
-      queue.push([ny, nx]);
-    } 
-  }
+    while(queue.length){
+        const [y, x] = queue.shift();
 
-  queue.push(jh);
-  j_visited[jh[0]][jh[1]] = 1;
-  
-  while(queue.length){
-    const [y, x] = queue.shift();
+        if(y === 0 || x === 0 || y === R - 1 || x === C - 1){
+            console.log(j_visited[y][x]);
+            return;
+        }
 
-    if(y === 0 || x === 0 || y === R-1 || x === C-1){
-      ret = j_visited[y][x];
-      break;
+        for (const d of dir){
+            const [ny, nx] = [y + d[0], x + d[1]];
+            
+            if(ny < 0 || nx < 0 || ny >= R || nx >= C || j_visited[ny][nx] || a[ny][nx] === '#') continue;
+            if(j_visited[y][x] + 1 < f_visited[ny][nx]){
+                j_visited[ny][nx] = j_visited[y][x] + 1;
+                queue.push([ny, nx]);
+            }
+        }
     }
 
-    for (const [dy, dx] of dir){
-      let ny = y + dy, nx = x + dx;
-
-      if(ny < 0 || nx < 0 || ny >=R || nx >= C || j_visited[ny][nx] || a[ny][nx] === '#') continue;
-      if(f_visited[ny][nx] <= j_visited[y][x] + 1) continue;
-
-      j_visited[ny][nx] = j_visited[y][x] + 1;
-      queue.push([ny, nx]);
-    }
-  }
-  
-  console.log(ret ? ret : 'IMPOSSIBLE');
-
+    console.log("IMPOSSIBLE");
 }
 
 answer();
-
-// 불, 지훈 위치 확인
-// 불 먼저 BFS
-// 지훈 BFS하면서 불보다 작다는 조건
