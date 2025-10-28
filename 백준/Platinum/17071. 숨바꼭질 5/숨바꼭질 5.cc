c283d7ede34ffd007d76f168398efb97bd80e8f0
SeokHwan Ko <kosukwhon@gmail.com>
2025-10-28 14:22:48 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int max_n = 500000;
-int n, k, turn = 1, ok, visited[2][max_n + 4];
+int n, k, flag, turn = 1;
+int visited[2][500004];
+queue<int> q;
 
 int main() {
   cin >> n >> k;
 
   if (n == k) {
-    cout << 0 << '\n';
+    cout << 0;
     return 0;
   }
 
-  queue<int> q;
-  q.push(n);
   visited[0][n] = 1;
+  q.push(n);
 
   while (q.size()) {
     k += turn;
+    if (k > 500000) break;
 
-    if (k > max_n) break;
     if (visited[turn % 2][k]) {
-      ok = true;
+      flag = 1;
       break;
     }
 
@@ -31,28 +31,26 @@ int main() {
       q.pop();
 
       for (int nx : {x - 1, x + 1, x * 2}) {
-        if (nx < 0 || nx > max_n || visited[turn % 2][nx]) continue;
+        if (nx < 0 || nx > 500000 || visited[turn % 2][nx]) continue;
+
         visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
 
         if (nx == k) {
-          ok = 1;
+          flag = 1;
           break;
         }
         q.push(nx);
       }
-      if (ok) break;
+      if (flag) break;
     }
-    if (ok) break;
+    if (flag) break;
     turn++;
   }
 
-  if (ok)
-    cout << turn << '\n';
+  if (flag)
+    cout << turn;
   else
-    cout << -1 << '\n';
+    cout << -1;
 
   return 0;
 }
-
-// 50만 -> 2차원 배열로 만들수 없음.
-// 홀 짝 상태로만 다시 만날 수 있는지 확인 가능
